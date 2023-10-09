2ff4c31707b2460ab02dc8a70d7e19a19c144244
RedBull-Ui <privatk45@gmail.com>
2023-10-09 22:25:46 +0000

    definition des variables d environnement

--- app.js
+++ app.js
@@ -8,19 +8,19 @@ const axios = require('axios'); // Importez Axios
 
 app.set('view engine', 'ejs');
 
-
+// Configuration de la connexion à la base de données en utilisant des variables d'environnement
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'alimentaire',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 
 connection.connect((err) => {
   if (err) {
-      console.error('Erreur de connexion à la base de données :', err);
+    console.error('Erreur de connexion à la base de données :', err);
   } else {
-      console.log('Connecté à la base de données MySQL');
+    console.log('Connecté à la base de données MySQL');
   }
 });
 
