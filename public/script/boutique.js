2e7f05a71696288bf26cfe1eb98c6791347a4078
RedBull-Ui <privatk45@gmail.com>
2023-11-24 10:26:45 +0000

    point rouge updated

--- public/script/boutique.js
+++ public/script/boutique.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
       const produitExiste = panier.some((produit) => produit.id === id);
 
       if (produitExiste) {
-        alert('Ce produit est déjà ajouté au panier.');
+        alert('Déjà ajouté au panier.');
         return;
       }
 
@@ -26,7 +26,37 @@ document.addEventListener('DOMContentLoaded', () => {
       // Enregistre le panier mis à jour dans le localStorage
       localStorage.setItem('panier', JSON.stringify(panier));
 
-      console.log('Produit ajouté au panier !', panier);
+      localStorage.setItem('pointRouge','on')
+      alert('Produit ajouté au panier !');
+      mettreAJourPointRouge();
     });
   });
+
+  function mettreAJourPointRouge() {
+    const pointRouge = document.getElementById('point-rouge');
+  
+    // Vérifiez si localStorage est disponible
+    if (typeof localStorage !== 'undefined') {
+      // Récupère le panier depuis le localStorage
+      const panier = JSON.parse(localStorage.getItem('panier')) || [];
+  
+      // Vérifie si le panier n'est pas vide
+      if (panier.length > 0) {
+        // Mettez 'pointRouge' sur 'on' et affichez l'élément
+        localStorage.setItem('pointRouge', 'on');
+        pointRouge.style.display = 'block';
+      } else {
+        // Aucun produit trouvé, mettez 'pointRouge' sur 'off' et masquez l'élément
+        localStorage.setItem('pointRouge', 'off');
+        pointRouge.style.display = 'none';
+      }
+    } else {
+      // Gérez le cas où localStorage n'est pas disponible
+      localStorage.setItem('pointRouge', 'off');
+      pointRouge.style.display = 'none';
+      console.error('localStorage is not available.');
+    }
+  }
+  
+  
 });
